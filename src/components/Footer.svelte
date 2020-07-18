cd1663d978d2cbaf465031da6fecaa7e8cb2c9e9
Leonardo Rivero <codingleonardo@gmail.com>
2020-07-18 14:49:35 -0400

    Añadir un Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+
+<style>
+  .Footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 4em;
+    background: #eaeaea;
+  }
+  .Footer-message,
+  .Footer-powered {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: inherit;
+  }
+  .Footer-message h1 {
+    font-size: 1.2em;
+  }
+  .Footer-message a {
+    font-weight: bold;
+  }
+  .Footer-powered {
+    font-size: 1.2em;
+  }
+  .Footer-powered img {
+    height: 2.2em;
+    margin-left: 0.8em;
+  }
+</style>
+
+<footer class="Footer">
+  <div class="container">
+    <div class="Footer-message">
+      <h1>
+        Diseñado con 💚 por
+        <a href="https://github.com/CodingLeonardo">@CodingLeonardo</a>
+      </h1>
+    </div>
+    <div class="Footer-powered">
+      Impulsado por
+      <img src="/images/svelte-logo.svg" alt="" />
+    </div>
+  </div>
+</footer>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,22 @@
 <script>
   import Header from "../components/Header.svelte";
+  import Footer from "../components/Footer.svelte";
 
   export let segment;
 </script>
 
 <style>
   main {
-    position: relative;
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
+    padding: 2em 0;
   }
 </style>
 
 <Header {segment} />
 
 <main>
-  <slot />
+  <div class="container">
+    <slot />
+  </div>
 </main>
+
+<Footer />
