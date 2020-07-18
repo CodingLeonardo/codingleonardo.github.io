667d640d4d2b7d078e3b436cf113dae970ee3f3c
Leonardo Rivero <codingleonardo@gmail.com>
2020-07-18 14:56:08 -0400

    Crear ruta una "/certificados"
    En esta ruta debe contener informacion en formato JSON y debera renderizar una lista de certificados.

---
+++ src/routes/certificados/_certificados.js
@@ -0,0 +1,124 @@
+const certificados = [
+  {
+    name: "Programacion Basica",
+    src: "./images/diploma-programacion-basica.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Marca Personal",
+    src: "./images/diploma-marca-personal.svg",
+    categories: ["Marketing", "Todos"],
+  },
+  {
+    name: "Desarrollo Web Online",
+    src: "./images/diploma-html5-css3.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Responsive Design",
+    src: "./images/diploma-responsive-design.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Git y Github",
+    src: "./images/diploma-git-github.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "CSS Grid Layout",
+    src: "./images/diploma-css-grid-layout.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Fundamentos de Javascript",
+    src: "./images/diploma-fundamentos-javascript.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Ecmascipt 6+",
+    src: "./images/diploma-ecmascript-6.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Motor V8 de Chrome",
+    src: "./images/diploma-javascript-navegador.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Javacript Profesional",
+    src: "./images/diploma-javascript-profesional.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "SPA Javacript",
+    src: "./images/diploma-spa-javascript.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Webpack",
+    src: "./images/diploma-webpack.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "PostCSS",
+    src: "./images/diploma-postcss.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Animaciones Web",
+    src: "./images/diploma-animaciones-web.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Prework",
+    src: "./images/diploma-prework.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "React",
+    src: "./images/diploma-react.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "React Router y Redux",
+    src: "./images/diploma-react-router-redux.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "NextJS",
+    src: "./images/diploma-next-js.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "GraphQL",
+    src: "./images/diploma-graphql.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "TailwindCSS",
+    src: "./images/diploma-tailwind-css.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Backend con NodeJS",
+    src: "./images/diploma-backend-js.svg",
+    categories: ["Programacion", "Todos"],
+  },
+  {
+    name: "Hablar en Publico",
+    src: "./images/diploma-hablar-en-publico.svg",
+    categories: ["CrecimientoProfesional", "Todos"],
+  },
+  {
+    name: "Storytelling",
+    src: "./images/diploma-storytelling.svg",
+    categories: ["CrecimientoProfesional", "Todos"],
+  },
+  {
+    name: "Arquitecto Frontend",
+    src: "./images/diploma-arquitecto.svg",
+    categories: ["Programacion", "Todos"],
+  },
+];
+
+export default certificados;
---
+++ src/routes/certificados/index.svelte
@@ -0,0 +1,43 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`certificados.json`)
+      .then(r => r.json())
+      .then(certificados => {
+        return { certificados };
+      });
+  }
+</script>
+
+<script>
+  export let certificados;
+
+  import Certificado from "../../components/Certificado.svelte";
+  console.log(certificados);
+</script>
+
+<style>
+  .Certificados {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    gap: 1em;
+    align-items: center;
+    justify-items: center;
+    width: 80%;
+    margin: 0 auto;
+  }
+</style>
+
+<svelte:head>
+  <title>CodingLeonardo | Certificados</title>
+</svelte:head>
+
+<div class="Certificados">
+  {#each certificados as certificado}
+    <Certificado
+      name={certificado.name}
+      src={certificado.src}
+      categories={certificado.categories} />
+  {:else}
+    <h1>Loading...</h1>
+  {/each}
+</div>
