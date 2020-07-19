d4d51d4cc3f3f7bb96563861641889ce5bf4342a
Leonardo Rivero <codingleonardo@gmail.com>
2020-07-18 20:46:17 -0400

    Agregar un Home
    Agregar un Home a el cual se pueda acceder desde "/", debe contener una historia

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,11 +3,70 @@
 </script>
 
 <style>
-
+  .Home h1,
+  h2 {
+    margin-bottom: 0.5em;
+    font-weight: bold;
+  }
+  .Home p {
+    margin-left: 0.8em;
+    margin-bottom: 0.8em;
+    font-size: 1.2em;
+  }
 </style>
 
 <svelte:head>
-  <title>Leonardo Rivero - Desarrollador Web</title>
+  <title>CodingLeonardo</title>
 </svelte:head>
 
-<div>Home</div>
+<div class="Home">
+  <h1>Un Poco Sobre mi</h1>
+  <p>
+    Hola soy Leonardo!, tengo 14 años de edad, soy estudiante de platzi, tengo
+    24 cursos y 2 Carreras aprobadas en platzi, la mayoria de los cursos que eh
+    aprobado son de Desarrollo Frontend, pero tambien de Diseño, Marketing y
+    Crecimiento Profesional.
+  </p>
+  <h2>Asi fue como comence a desarrollar</h2>
+  <p>
+    Cuando comence a programar fue a los 13 años mas o menos, uno de los
+    primeros cursos de programacion que hice fue uno de python2, en cual aprendi
+    algunas nociones basicas de la programacion que apenas lograba entender,
+    intentaba hacer juegos con python asi que busque un tutorial en youtube pero
+    me parecio muy dificil, asi que deje de programar por un tiempo como por 3
+    meses, luego de eso supe de una empresa llamada Platzi la cual era una
+    escuela online en la cual vendian cursos de programacion pero habia un curso
+    de programacion que si es gratis. El curso de llama "Curso de Programacion
+    Basica", este curso me dio una base mas cimentada de la programacion, no
+    solo me enseño las bases, tambien aprendi con proyectos.
+  </p>
+  <p>
+    Despues de 6 meses de haber aprobado el Curso de Programacion Basica me
+    entere de que Platzi estaba dando becas para los venezolanos, entonces, me
+    registre para que me dieran la beca, luego de haber esperado tres semanas
+    envie un correo a Platzi para saber si la beca me la ivan a dar o no, me
+    respondieron que las becas las estanban dando gradualmente. Luego de dos
+    meses me llego un correo de Platzi diciendome que me habian dado la beca.
+    Ahi fue donde comenzo mi aventura como desarrollador Frontend.
+  </p>
+  <p>
+    Ya me habian dado la Beca, pero no tenia una ruta clara de que cursos iba a
+    tomar, entonces me puse en contacto con el team platzi para que me guiaran y
+    explicaran que cursos tomar, el primer curso que me recomendaron fue el
+    Curso de Desarrollo Web en el que aprendi HTML y CSS con proyectos que me
+    daban el curso. Pasaban los meses y yo aprendia facilmente con los curso de
+    Platzi, a los 5 meses de haber adquirido la beca, ya habia conseguido
+    aprender las fundamentos de javascript. Ya me sentia capaz de crear mi
+    primera pagina web de portafolio en la cual mostraba todos los certificados
+    y habilidades que tenia hasta ese momento la habia hecho con HTML, CSS y Js
+    Vanilla, obviamente la pagina no esta optimizada por eso tardaba mucho al
+    cargar.
+  </p>
+  <p>
+    Luego de haber estudiado 1 año con Platzi ya me siento capaz de aprender
+    nuevos framework solo y crear proyectos personales. Actualmente me encuentro
+    desarrollando una pagina que te muestra una grafica de los indices de
+    coronavirus en Venezuela. Estoy pensando en que otros proyectos podria
+    desarrollar, probablemente desarrollare mi propio blog.
+  </p>
+</div>
