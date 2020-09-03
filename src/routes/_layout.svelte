f62d50f251bcd7b042d0cb243a5e10c17c91e154
Leonardo Rivero <codingleonardo@gmail.com>
2020-09-03 11:20:34 -0400

    Re-estructurar el Layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,13 +6,29 @@
 </script>
 
 <style>
-
+  .background {
+    background: linear-gradient(243.33deg, #80f08b 46.99%, #35cc44 99.77%);
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -100;
+  }
+  .grid {
+    display: grid;
+    width: 100%;
+    min-height: 100vh;
+    grid-auto-rows: max-content;
+  }
 </style>
 
-<Header {segment} />
+<div class="background" />
 
-<main>
-  <slot />
-</main>
-
-<Footer />
+<div class="grid">
+  <Header {segment} />
+  <main>
+    <slot />
+  </main>
+  <Footer />
+</div>
