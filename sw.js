6fc303d0faf5afa857cca967df31a5bcbf82df29
Leonardo Rivero <codingleonardo@gmail.com>
2021-01-18 07:20:37 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fca92205f44f81f70f15.js"
+    "url": "webpack-runtime-6b2343fd7f0df2d740b8.js"
   },
   {
     "url": "framework-beda7bde9d7dca0755ce.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "becd9ae625d88ca51ff75bfb0972cc21"
+    "revision": "fb25dbda8364afb9afe2e2cfdf1b7cac"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
